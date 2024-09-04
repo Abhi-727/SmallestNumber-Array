d3be4f06b5866bae2d4d30bcf5ffcfab13abfe05
Abhi-727 <chalamcharlaa3@gmail.com>
2024-09-04 22:42:43 +0530

    foreach Example

---
+++ TestForEach.java
@@ -0,0 +1,11 @@
+package com.org;
+
+
+public class TestForEach {
+	public static void main(String[] args) {
+		int num[] = {2,3,4,5,6};
+		for(int number : num) {
+			System.out.print(number + " ");
+		}
+	}
+}
